88366c3a5523aa6f52ae1a7cff10ba511fc640d5
Anton udén <udenanton@hotmail.com>
2022-04-20 14:04:59 +0200

    Prevent admin ui from going into sleep mode

---
+++ www_app/js/nosleep.js
@@ -0,0 +1,7 @@
+const noSleep = new NoSleep();
+
+document.addEventListener('click', function enableNoSleep() {
+	document.removeEventListener('click', enableNoSleep, false);
+	noSleep.enable();
+	console.log("NoSleep enabled");
+}, false);
