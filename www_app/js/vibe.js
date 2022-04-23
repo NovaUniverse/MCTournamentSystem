796f84b4cb8175b99992712fa18aedcc9abe6871
Anton udén <udenanton@hotmail.com>
2022-04-23 12:40:11 +0200

    aaaaaaaaaaaaaa

---
+++ www_app/js/vibe.js
@@ -0,0 +1,49 @@
+var hasVibe = false;
+function vibe() {
+	if (hasVibe) {
+		return;
+	}
+	hasVibe = true;
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
+	var player;
+}
+
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('yt_player', {
+		videoId: 'Q_5HCPgFgW8',
+		controls: 0,
+		disablekb: 1,
+		playerVars: {
+			'playsinline': 1,
+		},
+		events: {
+			'onReady': function (event) {
+				event.target.playVideo();
+			}
+		}
+	});
+}
+
+$(() => {
+	window.addEventListener('keypress', (function () {
+		var strToType = 'vibe',
+			strTyped = '';
+		return function (event) {
+			var character = String.fromCharCode(event.which);
+			strTyped += character;
+			if (strToType.indexOf(strTyped) === -1) {
+				strTyped = '';
+			} else if (strTyped === strToType) {
+				strTyped = '';
+				vibe();
+			}
+		};
+	}()));
+});
