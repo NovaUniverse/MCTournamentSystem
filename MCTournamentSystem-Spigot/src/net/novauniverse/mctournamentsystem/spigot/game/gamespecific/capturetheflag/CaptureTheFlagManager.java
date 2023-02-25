3a0eb33489819d32e32f045dc537fc528c098980
Anton udén <udenanton@hotmail.com>
2023-02-25 12:54:08 +0100

    Update CaptureTheFlagManager.java

--- MCTournamentSystem-Spigot/src/net/novauniverse/mctournamentsystem/spigot/game/gamespecific/capturetheflag/CaptureTheFlagManager.java
+++ MCTournamentSystem-Spigot/src/net/novauniverse/mctournamentsystem/spigot/game/gamespecific/capturetheflag/CaptureTheFlagManager.java
@@ -6,15 +6,57 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import net.md_5.bungee.api.ChatColor;
+import net.novauniverse.capturetheflag.NovaCaptureTheFlag;
+import net.novauniverse.capturetheflag.game.CaptureTheFlag;
 import net.novauniverse.capturetheflag.game.event.FlagCapturedEvent;
+import net.novauniverse.mctournamentsystem.spigot.TournamentSystem;
 import net.novauniverse.mctournamentsystem.spigot.score.ScoreManager;
+import net.zeeraa.novacore.commons.tasks.Task;
+import net.zeeraa.novacore.commons.utils.TextUtils;
 import net.zeeraa.novacore.spigot.module.NovaModule;
+import net.zeeraa.novacore.spigot.module.modules.scoreboard.NetherBoardScoreboard;
+import net.zeeraa.novacore.spigot.tasks.SimpleTask;
 
 public class CaptureTheFlagManager extends NovaModule implements Listener {
+	public static int SUDDEN_DEATH_COUNTDOWN_LINE = 5;
 	public static int FLAG_CAPTURE_SCORE = 20;
-	
+
+	private Task task;
+
 	public CaptureTheFlagManager() {
 		super("TournamentSystem.GameSpecific.CaptureTheFlagManager");
+	}
+
+	@Override
+	public void onLoad() {
+		task = new SimpleTask(TournamentSystem.getInstance(), new Runnable() {
+			@Override
+			public void run() {
+				CaptureTheFlag game = NovaCaptureTheFlag.getInstance().getGame();
+				if (game.hasStarted()) {
+					if (!TournamentSystem.getInstance().isDisableScoreboard()) {
+						if (!game.isSuddenDeathActive()) {
+							if (game.getSuddenDeathTask() != null) {
+								int secondsLeft = (int) (game.getSuddenDeathTask().getMSLeft() / 1000);
+								NetherBoardScoreboard.getInstance().setGlobalLine(SUDDEN_DEATH_COUNTDOWN_LINE, ChatColor.GOLD + "Sudden death: " + ChatColor.AQUA + TextUtils.secondsToTime(secondsLeft));
+							}
+						} else {
+							NetherBoardScoreboard.getInstance().clearGlobalLine(SUDDEN_DEATH_COUNTDOWN_LINE);
+						}
+					}
+				}
+			}
+		}, 10L);
+	}
+
+	@Override
+	public void onEnable() throws Exception {
+		Task.tryStartTask(task);
+	}
+
+	@Override
+	public void onDisable() throws Exception {
+		Task.tryStopTask(task);
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
