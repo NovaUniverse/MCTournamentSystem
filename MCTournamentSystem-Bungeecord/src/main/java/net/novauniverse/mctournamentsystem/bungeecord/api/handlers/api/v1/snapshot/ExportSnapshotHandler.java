40e4b5f83c98d00bdaf663c99a27f2a34a05179a
Anton udén <udenanton@hotmail.com>
2023-08-18 15:47:56 +0200

    update score import feature

--- MCTournamentSystem-Bungeecord/src/main/java/net/novauniverse/mctournamentsystem/bungeecord/api/handlers/api/v1/snapshot/ExportSnapshotHandler.java
+++ MCTournamentSystem-Bungeecord/src/main/java/net/novauniverse/mctournamentsystem/bungeecord/api/handlers/api/v1/snapshot/ExportSnapshotHandler.java
@@ -24,58 +24,64 @@ public class ExportSnapshotHandler extends TournamentEndpoint {
 		JSONObject result = new JSONObject();
 		JSONArray players = new JSONArray();
 		JSONArray teams = new JSONArray();
-		
+
 		{
 			String sql = "SELECT s.server AS server, s.reason AS reason, s.amount AS amount, s.gained_at AS gained_at, p.uuid AS uuid FROM player_score AS s LEFT JOIN players AS p ON p.id = s.player_id";
 			PreparedStatement ps = TournamentSystemCommons.getDBConnection().getConnection().prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String uuid = rs.getString("uuid");
 				String server = rs.getString("server");
 				String reason = rs.getString("reason");
 				int amount = rs.getInt("amount");
 				String gainedAt = rs.getString("gained_at");
-				
+
 				JSONObject entry = new JSONObject();
-				
+
 				entry.put("uuid", uuid);
 				entry.put("server", server);
 				entry.put("reason", reason);
 				entry.put("amount", amount);
 				entry.put("gained_at", gainedAt);
-				
+
 				players.put(entry);
 			}
+
+			rs.close();
+			ps.close();
 		}
-		
+
 		{
 			String sql = "SELECT s.server AS server, s.reason AS reason, s.amount AS amount, s.gained_at AS gained_at, t.team_number AS team_number FROM team_score AS s LEFT JOIN teams AS t ON t.id = s.team_id";
 			PreparedStatement ps = TournamentSystemCommons.getDBConnection().getConnection().prepareStatement(sql);
 			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int teamNumber = rs.getInt("team_number");
 				String server = rs.getString("server");
 				String reason = rs.getString("reason");
 				int amount = rs.getInt("amount");
 				String gainedAt = rs.getString("gained_at");
-				
+
 				JSONObject entry = new JSONObject();
-				
+
 				entry.put("team_number", teamNumber);
 				entry.put("server", server);
 				entry.put("reason", reason);
 				entry.put("amount", amount);
 				entry.put("gained_at", gainedAt);
-				
+
 				teams.put(entry);
 			}
+
+			rs.close();
+			ps.close();
 		}
-		
+
 		result.put("players", players);
 		result.put("teams", teams);
-		
+
 		return new JSONResponse(result);
 	}
 }
