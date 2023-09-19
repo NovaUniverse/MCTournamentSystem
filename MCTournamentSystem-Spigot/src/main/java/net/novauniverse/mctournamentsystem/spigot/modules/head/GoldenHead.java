aca2a039cc444bd894dfb4da767b5c76b72149f1
Anton udén <udenanton@hotmail.com>
2023-09-19 13:15:40 +0200

    check if golden head is in main hand

--- MCTournamentSystem-Spigot/src/main/java/net/novauniverse/mctournamentsystem/spigot/modules/head/GoldenHead.java
+++ MCTournamentSystem-Spigot/src/main/java/net/novauniverse/mctournamentsystem/spigot/modules/head/GoldenHead.java
@@ -1,7 +1,6 @@
 package net.novauniverse.mctournamentsystem.spigot.modules.head;
 
 import io.github.bananapuncher714.nbteditor.NBTEditor;
-import net.novauniverse.mctournamentsystem.spigot.modules.cooldown.TickCooldown;
 import net.novauniverse.mctournamentsystem.spigot.textures.Textures;
 import net.zeeraa.novacore.spigot.abstraction.VersionIndependentUtils;
 import net.zeeraa.novacore.spigot.abstraction.enums.VersionIndependentSound;
@@ -73,26 +72,27 @@ public class GoldenHead extends NovaModule implements Listener {
 
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (e.getItem() != null) {
-			if (NBTEditor.contains(e.getItem(), "tournamentsystem", "goldenhead")) {
-				if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-					e.setCancelled(true);
+		if (VersionIndependentUtils.get().isInteractEventMainHand(e)) {
+			if (e.getItem() != null) {
+				if (NBTEditor.contains(e.getItem(), "tournamentsystem", "goldenhead")) {
+					if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+						e.setCancelled(true);
 
-					Player player = e.getPlayer();
+						Player player = e.getPlayer();
 
+						if (!CooldownManager.get().isActive(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID)) {
+							CooldownManager.get().set(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID, 100);
+							InventoryUtils.removeOneFromHand(e.getPlayer());
+							// p.getWorld().playSound(p.getLocation(), Sound.EAT, 1F, 1F);
+							VersionIndependentSound.EAT.play(player);
 
-					if (!CooldownManager.get().isActive(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID)) {
-						CooldownManager.get().set(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID, 100);
-						InventoryUtils.removeOneFromHand(e.getPlayer());
-						// p.getWorld().playSound(p.getLocation(), Sound.EAT, 1F, 1F);
-						VersionIndependentSound.EAT.play(player);
-
-						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 30 * 20, 1));
-						player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 15 * 20, 1));
-						player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 60 * 20, 1));
-					} else {
-						double seconds = ((double)CooldownManager.get().getTimeLeft(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID) / 20.0);
-						player.sendMessage(ChatColor.RED + "Wait " + seconds + (seconds == 1 ? " second" : "seconds") + " to eat another Golden Head");
+							player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 30 * 20, 1));
+							player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 15 * 20, 1));
+							player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 60 * 20, 1));
+						} else {
+							double seconds = ((double) CooldownManager.get().getTimeLeft(player.getUniqueId(), GOLDEN_HEAD_COOLDOWN_ID) / 20.0);
+							player.sendMessage(ChatColor.RED + "Wait " + seconds + (seconds == 1 ? " second" : "seconds") + " to eat another Golden Head");
+						}
 					}
 				}
 			}
