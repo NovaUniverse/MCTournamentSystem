a01509ae709c04a7c6a81a7c1ed66d812bc653f1
Anton udén <udenanton@hotmail.com>
2023-11-03 01:06:06 +0100

    remove old admin ui

--- www_app/js/enforce_login.js
+++
@@ -1,7 +0,0 @@
-$(() => {
-	$.getJSON("/api/v1/user/whoami", (data) => {
-		if(!data.logged_in) {
-			window.location = "/app/login?redirect=" + encodeURIComponent(window.location.href);
-		}
-	});
-})
--- www_app/js/preloader_settings.js
+++
@@ -1,8 +0,0 @@
-__tnPreloader_setOptions__ = {
-	custom_image: '/app/img/loading.webp',
-	custom_text: 'Loading...',
-	custom_text_color: '#ffffff',
-	background_is_img: false,
-	custom_background: '#111117',
-	text_placement: 'column'
-}
